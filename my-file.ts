abbc6a3517369c414479a128c0c389d4fac7d251
Sophie LAURENT <sophielaurent92@gmail.com>
2024-01-14 14:26:19 +0100

    Utility Types

--- my-file.ts
+++ my-file.ts
@@ -10,17 +10,6 @@ console.log(result); */
 
  const message: string = sayHello({ firstName: 'World', money: 123 }); */
 
-
- // Ce code TypeScript n'est pas complet.
-// Regardez les dernières lignes de ce script : TypeScript remonte des erreurs.
-// Votre but est de modifier le code pour corriger ces erreurs.
-// Vous allez devoir modifier les types déjà définis et en créer d'autres.
-// ====================
-
-type WithMagic = {
-    magic: number;
- };
-
 type Character = {
     name: string;
     life: number;
@@ -28,85 +17,86 @@ type Character = {
     defense: number;
  };
  
- type Pet = Character;
+  type Stats = Omit<Character, "name">;
  
- type Hero = Character & {
-    pets?: Array<Pet>;
-    equipments?: Array<Equipment>;
- };
-
- type MagicalPet = Pet & WithMagic ;
-
- type Equipment = {
+ type Equipment = Omit<Partial<Stats>, "life"> & {
     price: number;
-    attack?: number;
-    defense?: number;
  };
-
- type Magician = Hero & WithMagic ;
+ 
+ type WithMagic<T> = T & {
+    magic: number;
+ }
  
  // ====================
- // Ne touchez PAS aux lignes ci-dessous, faites en sorte que TypeScript
- // soit content en modifiant UNIQUEMENT le code du dessus !
- 
- const babyDragon: Pet = {
-    name: 'Typeon',
-    life: 50,
-    attack: 10,
-    defense: 20,
- };
- 
- const babyLicorn: MagicalPet = {
-    name: 'Typemoor',
-    life: 80,
-    attack: 5,
-    defense: 30,
-    magic: 50,
- };
- 
- const smallArmor: Equipment = {
-    price: 30,
-    attack: 2,
-    defense: 5,
- };
- 
- const smallSword: Equipment = {
-    price: 20,
-    attack: 5,
- };
- 
- const smallShield: Equipment = {
-    price: 15,
-    defense: 10,
- };
- 
- const smallMagicalWand: Equipment & WithMagic = {
-    price: 50,
-    attack: 4,
-    magic: 10,
- };
- 
- const myWarriorHero: Hero = {
-    name: 'TSolomon',
-    life: 200,
-    attack: 30,
-    defense: 50,
-    pets: [babyDragon, babyLicorn],
-    equipments: [
-       smallArmor,
-       smallSword,
-       smallShield,   
-    ],
- };
- 
- const myMagicienHero: Magician = {
-    name: 'TSoren',
-    life: 150,
-    attack: 20,
-    defense: 40,
-    equipments: [smallMagicalWand],
-    magic: 200,
- };
+
+type Pet = Character;
+type MagicalPet = WithMagic<Pet>;
+
+type Hero = Character & {
+   pets?: Array<Pet>;
+   equipments?: Array<Equipment>;
+};
+
+type Magician = WithMagic<Hero>;
+
+const babyDragon: Pet = {
+   name: 'Typeon',
+   life: 50,
+   attack: 10,
+   defense: 20,
+};
+
+const babyLicorn: MagicalPet = {
+   name: 'Typemoor',
+   life: 80,
+   attack: 5,
+   defense: 30,
+   magic: 50,
+};
+
+const smallArmor: Equipment = {
+   price: 30,
+   attack: 2,
+   defense: 5,
+};
+
+const smallSword: Equipment = {
+   price: 20,
+   attack: 5,
+};
+
+const smallShield: Equipment = {
+   price: 15,
+   defense: 10,
+};
+
+const smallMagicalWand: WithMagic<Equipment> = {
+   price: 50,
+   attack: 4,
+   magic: 10,
+};
+
+const myWarriorHero: Hero = {
+   name: 'TSolomon',
+   life: 200,
+   attack: 30,
+   defense: 50,
+   pets: [babyDragon, babyLicorn],
+   equipments: [
+      smallArmor,
+      smallSword,
+      smallShield,   
+   ],
+};
+
+const myMagicienHero: Magician = {
+   name: 'TSoren',
+   life: 150,
+   attack: 20,
+   defense: 40,
+   equipments: [smallMagicalWand],
+   magic: 200,
+};
 
 
 export{}
